af74b02f59d6d912001abb907f1ddd3b505f2bb5
Joakim Eriksson <joakime@sics.se>
2020-05-27 22:45:12 +0200

    added Telnet example to Main

--- src/main/java/com/botbox/bbsh/Main.java
+++ src/main/java/com/botbox/bbsh/Main.java
@@ -1,11 +1,20 @@
 package com.botbox.bbsh;
 
 import com.botbox.bbsh.jline.ConsoleCLIContext;
+import org.jline.builtins.telnet.Telnet;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+import java.io.PrintStream;
+import java.util.Map;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         CLI cli = new CLI();
+
+        /* Exec command is not among the default commands that are automatically registered */
+        cli.registerCommand(ExecCommand.class);
+
         CLIContext cliContext;
         boolean useJlineFallback = false;
         if (useJlineFallback && "xterm".equalsIgnoreCase(System.getenv("TERM"))) {
@@ -18,8 +27,32 @@ public class Main {
         cliContext.setPrompt("bbsh> ");
         cliContext.start();
 
+        /* Also setup a telnet server */
+        Terminal terminal = TerminalBuilder.builder().name("BBSH").build();
+        Telnet telnet = new Telnet(terminal, new Telnet.ShellProvider() {
+            @Override
+            public void shell(Terminal terminal, Map<String, String> environment) {
+                CLIContext cliContext2;
+                terminal.echo(true);
+                System.out.println("Shell Terminal:" + terminal.echo(true));
+                cliContext2 = new StreamCLIContext(cli, terminal.input(), new PrintStream(terminal.output()), new PrintStream(terminal.output()));
+                cliContext2.setPrompt("bbsh>");
+                cliContext2.start();
+                for (;;) {
+                    try {
+                        Thread.sleep(60000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        telnet.telnetd(new String[] {"telnetd", "-p", "2700", "start"});
+
         for (;;) {
             try {
+                System.out.println("sleep...");
                 Thread.sleep(60000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
